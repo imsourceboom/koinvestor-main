34c0730e3765452ecf03588ca235ccd4185696df
imsourceboom <ecmascript.developer@gmail.com>
2019-03-13 16:43:43 +0900

    service worker register test

--- src/es6/index.js
+++ src/es6/index.js
@@ -8,7 +8,7 @@ import './menu';
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
