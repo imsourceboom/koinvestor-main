d79e7cf67322682fceb3afbd953ac94ae587bef6
imsourceboom <ecmascript.developer@gmail.com>
2019-03-13 13:16:17 +0900

    header scrollreveal animation detail apply

--- src/es6/scrollreveal/index.js
+++ src/es6/scrollreveal/index.js
@@ -9,6 +9,31 @@ sr.reveal('main > section', {
     duration: 1000,
 });
 
-sr.reveal('main > section li', {
+
+// sr.reveal('main header', {
+//     duration: 1000,
+//     distance: '60px',
+//     reset: false
+// });
+
+sr.reveal('main header figure', {
     duration: 1000,
+    origin: 'left',
+    distance: '60px',
+    reset: false
 });
+
+sr.reveal('main header .hamburger', {
+    duration: 1000,
+    origin: 'right',
+    distance: '60px',
+    reset: false
+});
+
+for (var i = 100; i < 1000; i += 100) {
+    sr.reveal('.delay-' + i, {
+        origin: 'top',
+        delay: i,
+        reset: false
+    });
+}
