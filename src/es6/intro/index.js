0c041dfd8124e423006ec09d898be3085e35ed56
imsourceboom <ecmascript.developer@gmail.com>
2019-03-22 18:22:38 +0900

    intro test

--- src/es6/intro/index.js
+++ src/es6/intro/index.js
@@ -9,37 +9,43 @@ import {
 
 const bodyEle = body.self;
 const mainEle = main.self;
+const headerEle = header.self;
 const aboutEle = about.self;
 const cover = intro.self;
 const typed = intro.typed;
 const arrow = intro.arrow;
 
-const headerEle = header.self;
+
 const li = Array.from(header.li);
 const logo = header.logo;
 const hamburger = header.hamburger;
 
 
+
+
 window.addEventListener('scroll', () => {
+
+    let coverNone;
     if (cover.style.display !== 'none') {
         typed.classList.add('trigger');
         arrow.classList.add('disappear');
 
-
-        setTimeout(() => {
+        coverNone = setTimeout(() => {
             cover.style.display = 'none';
-            setTimeout(() => {
-                if (cover.style.display === 'none') {
-                    window.scrollTo(0, 0);
-                    mainEle.classList.add('active');
-                    headerEle.classList.add('active');
-                    li.map((e) => {
-                        e.classList.add('reveal');
-                    })
-                    logo.classList.add('active');
-                    hamburger.classList.add('active');
-                }
-            })
+            window.scrollTo(0, 0);
+            mainEle.classList.add('active');
+            headerEle.classList.add('active');
         }, 1500);
+
+    } else {
+        clearTimeout(coverNone);
+        // window.scrollTo(0, 0);
+        // mainEle.classList.add('active');
+        // headerEle.classList.add('active');
+        li.map((e) => {
+            e.classList.add('reveal');
+        })
+        logo.classList.add('active');
+        hamburger.classList.add('active');
     }
-})
+});
